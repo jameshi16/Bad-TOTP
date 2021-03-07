efd1390ef900d47a740cc890441c1c18e9a38694
James <lim.en.hui.james@gmail.com>
2021-03-07 18:57:21 +0800

    Support pure C compilers

--- totp.h
+++ totp.h
@@ -10,7 +10,7 @@
 #define T0 0
 #define X 30 // time step
 
-uint32_t totp(hotp_context *ctx, uint8_t digits = 6) {
+uint32_t totp(hotp_context *ctx, uint8_t digits) {
   time_t now = time(NULL);
   ctx->counter = (now - T0) / X;
   
